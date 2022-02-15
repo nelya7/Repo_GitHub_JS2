7622d58a6ed8616e0fa2e67f3c4ff3c0903bc4a0
nelya7 <88278527+nelya7@users.noreply.github.com>
2022-02-15 21:27:38 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const products = [
-  { id: 1, title: 'Notebook', price: 1000 },
-  { id: 2, title: 'Mouse', price: 100 },
+  { id: 1, title: 'Notebook', price: 1000, },
+  { id: 2, title: 'Mouse', price: 100, },
   { id: 3, title: 'Keyboard', price: 250 },
   { id: 4, title: 'Gamepad', price: 150 }
 

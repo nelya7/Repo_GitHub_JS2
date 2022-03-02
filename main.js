c33b2565058340e9968cb647d611c3d41969e0cc
nelya7 <88278527+nelya7@users.noreply.github.com>
2022-03-02 20:00:34 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,109 +1,77 @@
-'use strict';
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
-class ProductList {
-    constructor(container = '.products') { // параметр container обозначает блок, в который мы хотим поместить наши товары products (селектор из вёрстки .products)
-        this._container = document.querySelector(container); // Если в переменной(поле класса) стоит this. это значит, что данная переменная(поле класса) доступна в каждом методе
-        this._goods = []; //поле класса//
-        this._allProducts = []; //поле класса//
-
-        this._fetchGoods(); //поле класса//
-        this._render(); //поле класса//
-    }
-
-    _fetchGoods() { //метод класса//
-        this._goods = [
-            { id: 1, title: 'Notebook', price: 20000 },
-            { id: 2, title: 'Mouse', price: 1500 },
-            { id: 3, title: 'Keyboard', price: 5000 },
-            { id: 4, title: 'Gamepad', price: 4500 },
-        ];
-    }
-
-    _render() { //метод класса//
-        for (const product of this._goods) {
-            const productObject = new ProductItem(product);
-
-            this._allProducts.push(productObject);
-            this._container.insertAdjacentHTML('beforeend', productObject.getHTMLString());
+const app = new Vue({
+    el: '#app',
+    data: {
+        userSearch: '',
+        showCart: false,
+        catalogUrl: '/catalogData.json',
+        cartUrl: '/getBasket.json',
+        cartItems: [],
+        filtered: [],
+        imgCart: 'https://via.placeholder.com/50x100',
+        products: [],
+        imgProduct: 'https://via.placeholder.com/200x150'
+    },
+    methods: {
+        getJson(url){
+            return fetch(url)
+                .then(result => result.json())
+                .catch(error => console.log(error))
+        },
+        addProduct(item){
+            this.getJson(`${API}/addToBasket.json`)
+                .then(data => {
+                    if(data.result === 1){
+                       let find = this.cartItems.find(el => el.id_product === item.id_product);
+                       if(find){
+                           find.quantity++;
+                       } else {
+                           const prod = Object.assign({quantity: 1}, item);//создание нового объекта на основе двух, указанных в параметрах
+                           this.cartItems.push(prod)
+                       }
+                    }
+                })
+        },
+        remove(item){
+            this.getJson(`${API}/addToBasket.json`)
+                .then(data => {
+                    if (data.result === 1) {
+                        if(item.quantity>1){
+                            item.quantity--;
+                        } else {
+                            this.cartItems.splice(this.cartItems.indexOf(item), 1);
+                        }
+                    }
+                })
+        },
+        filter(){
+            let regexp = new RegExp(this.userSearch, 'i');
+            this.filtered =  this.products.filter(el => regexp.test(el.product_name));
         }
+    },
+    mounted(){
+        this.getJson(`${API + this.cartUrl}`)
+            .then(data => {
+                for (let item of data.contents){
+                    this.cartItems.push(item);
+                }
+            });
+        this.getJson(`${API + this.catalogUrl}`)
+            .then(data => {
+                for (let item of data){
+                    this.$data.products.push(item);
+                    this.$data.filtered.push(item);
+                }
+            });
+        this.getJson(`getProducts.json`)
+            .then(data => {
+                for(let item of data){
+                    this.products.push(item);
+                    this.filtered.push(item);
+                }
+            })
     }
 
-    getSum() {
-        for (let product of this.goods) {
-            s += product.price;
-        }
-    }
-}
+});
 
-
-class ProductItem {
-    constructor(product, img = 'https://via.placeholder.com/200x150') {
-        this.id = product.id;
-        this.title = product.title;
-        this.price = product.price;
-        this.img = img;
-    }
-
-    getHTMLString() {
-        return `<div class="product-item" data-id="${this.id}">
-              <img src="${this.img}" alt="Some img">
-              <div class="desc">
-                  <h3>${this.title}</h3>
-                  <p>${this.price} \u20bd</p>
-                  <button class="buy-btn">Купить</button>
-              </div>
-          </div>`;
-    }
-}
-
-let list = new ProductsList();
-list.render();
-list.getSum();
-
-class Basket {
-    addGoods() {
-
-    }
-    removeGoods() {
-
-    }
-    changeGoods() {
-
-    }
-
-    render() {
-
-    }
-}
-
-class ElemBasket {
-    render() { }
-
-}
-
-// const products = [
-//   {id: 1, title: 'Notebook', price: 20000},
-//   {id: 2, title: 'Mouse', price: 1500},
-//   {id: 3, title: 'Keyboard', price: 5000},
-//   {id: 4, title: 'Gamepad', price: 4500},
-// ];
-//
-// const renderProduct = (item, img='https://via.placeholder.com/200x150') => `<div class="product-item" data-id="${this.id}">
-//               <img src="${img}" alt="Some img">
-//               <div class="desc">
-//                   <h3>${item.title}</h3>
-//                   <p>${item.price} \u20bd</p>
-//                   <button class="buy-btn">Купить</button>
-//               </div>
-//           </div>`;
-//
-// const renderProducts = list => {
-//   document.querySelector('.products')
-//       .insertAdjacentHTML(
-//           'beforeend',
-//           list.map(item => renderProduct(item)).join('')
-//       );
-// };
-//
-// renderProducts(products);
-//
